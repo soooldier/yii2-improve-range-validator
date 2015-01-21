0a25603e2173cad15d43804a6ec2071d30109e4b
soooldier <soooldier@live.com>
2015-01-21 23:41:27 +0800

    修改验证bug

--- ImproveRangeValidator.php
+++ ImproveRangeValidator.php
@@ -44,6 +44,9 @@ class ImproveRangeValidator extends RangeValidator
             $this->range = call_user_func($this->advanceRange, $this->range, $model);
         }
         $tmp_value = explode($this->sep, $model->$attribute);
-        return $this->validateValue($tmp_value);
+        $result = $this->validateValue($tmp_value);
+        if (!empty($result)) {
+            $this->addError($model, $attribute, $result[0], $result[1]);
+        }
     }
 }
